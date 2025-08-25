7085c4c4d8a94cb032c70b642551b4ec29c3ad0b
taraaubrey <tara.a.forstner@gmail.com>
2025-08-25 17:31:00 +1200

    updated glm

--- scripts/d_pest_GLM.py
+++ scripts/d_pest_GLM.py
@@ -5,28 +5,21 @@ from setup import *
 
 def main():
 
-    pst_name = f"{MODEL_NAME}.pst"
+    pst_name = f"{MODEL_NAME}_pp.pst"
     pst = pyemu.Pst(os.path.join(TEMP_DIR, pst_name))
     pst.control_data.noptmax = -1
-
-    # gte rid of high dimensional parameterization
-    par = pst.parameter_data
-    gr_pars = par.loc[par.pargp.apply(lambda x: "gr" in x and "sfr" not in x),"parnme"]
-    par.loc[gr_pars,"partrans"] = "fixed"
-
 
     # write a new pst file
     pst.write(os.path.join(TEMP_DIR, pst_name))
     
-
     num_workers = os.cpu_count()
 
     # the master directory
-    m_d=os.path.join(os.path.join(TEMP_DIR, '..'), 'master_mc')
+    m_d=os.path.join(os.path.join(TEMP_DIR, '..'), 'master_glm_1')
 
     pyemu.os_utils.start_workers(
         worker_dir=TEMP_DIR, # the folder which contains the "template" PEST dataset
-        exe_rel_path=f'pestpp-swp', #the PEST software version we want to run
+        exe_rel_path=f'pestpp-glm', #the PEST software version we want to run
         pst_rel_path=pst_name, # the control file to use with PEST
         num_workers=num_workers, #how many agents to deploy
         worker_root= os.path.join(TEMP_DIR, '..'),
